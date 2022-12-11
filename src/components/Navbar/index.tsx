b398145349b49844e9134f936bacb4a8658be177
stoikiy <mrstoikiy@gmail.com>
2022-12-11 15:51:03 +0400

    fixes for deploy

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react';
-import classNames from 'classnames';
+import Link from 'next/link'
 
 import styles from './index.module.scss';
 
-const NavBar: React.FC = () => {
-    return (
-        <React.Fragment>
-            <nav className={styles.app_nav}>
-                <a href="/" className={styles.link}>Home</a>
-                <a href="/" className={styles.link}>About</a>
-                <a href="/" className={styles.link}>Contact</a>
-            </nav>
-        </React.Fragment>
-    )
-}
+const NavBar: React.FC = () => (
+    <React.Fragment>
+        <nav className={styles.app_nav}>
+
+            <Link href="/" className={styles.link}>Home</Link>
+            <Link href="/" className={styles.link}>About</Link>
+            <Link href="/" className={styles.link}>Contact</Link>
+        </nav>
+    </React.Fragment>
+);
 
 export default NavBar;
